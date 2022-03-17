ce1607386c5ab0f11bfb9468c8851dbb17b02bed
teslatrader <2724546@gmail.com>
2022-03-17 23:49:34 +0700

    1st commit

---
+++ bot.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import requests, datetime, time
+
+class TelegramBot:
+    def __init__(self, token, chat_address):
+        pass
+        self.token = token
+        self.message = ''
+        self.chat_id = chat_address
+        self.send_msg_temp = f'/sendMessage?chat_id={self.chat_id}'
+        self.parse_mode = '&parse_mode=MarkdownV2&text='
+        self.bot_url = f'https://api.telegram.org/bot{self.token}{self.send_msg_temp}{self.parse_mode}'
+
+
+    def send_msg(self, message):
+        # print(message)
+        self.message = self.msg_cast(f'{datetime.datetime.fromtimestamp(time.time())}: {message}')
+        self.send_it()
+        return
+
+
+    def send_it(self):
+        url = f'{self.bot_url}{self.message}'
+        r = requests.get(url)
+        return
+
+
+    def msg_cast(self, message: str):
+        message = str(message)
+        for char in ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!' ]:
+            if char in message:
+                message = message.replace(char, "\\"+char)
+        return message
---
+++ readme.md
@@ -0,0 +1,38 @@
+# SIMPLE TELEGRAM NOTIFIER BOT
+
+*Description*
+
+Bot sends to particular telegram chat any message, which is previously processed
+about specific symbols.
+
+Main goal of that bot - easiest way to use it like telegram notifier in another
+more complex bots.
+
+**Instructions**
+
+Make sure that you have installed python v.3.7+
+
+Open terminal in root directory
+
+Activate virtual environment (you have to have virtualenv installed)
+or install packages from requirements.txt globally:
+
+  for activating existing venv:
+  ```
+  . venv/bin/activate
+  ```
+
+  install requirements:
+  ```
+  pip3 install -r requirements.txt
+  ```
+
+**Usage**
+
+    from bot import TelegramBot as tgb
+    import bot_token, time
+
+    tb = tgb(bot_token.TOKEN, bot_token.CHAT_ID)
+    tb.send_msg(f'\n{time.ctime()}: initializing bot...')
+
+***Notice that bots api token and chat id should be inserted in bot_token.py***
